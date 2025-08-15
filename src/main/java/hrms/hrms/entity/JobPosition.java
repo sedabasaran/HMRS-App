0efce67e51ff945aae0cf75e8c5a5b4f7ffa41cf
sedabasaran <seda_basaran_@hotmail.com>
2025-08-15 21:54:40 +0300

    HMRS-App

--- src/main/java/hrms/hrms/entity/JobPosition.java
+++ src/main/java/hrms/hrms/entity/JobPosition.java
@@ -3,7 +3,6 @@ package hrms.hrms.entity;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
